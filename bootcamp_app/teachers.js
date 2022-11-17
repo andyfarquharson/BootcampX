3acc4cfc6a6b91431dd464c548faff2bd0a9d16d
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-17 03:39:38 +0000

    Prevent SQL injection with parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -18,7 +18,7 @@ client.connect()
     console.log('DB Connection Error', error)
     client.end()
   })
-
+const cohort = process.argv[2];
 
 client.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -26,11 +26,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [cohort])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
+  client.end();
 });
